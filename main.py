7c961d458458d3a3c6a0363cace30a3a9629743c
Enrico Da Rodda <enricodarodda@hotmail.it>
2019-11-24 23:35:26 +0100

    Solve iss013 - Refactoring main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+#! /usr/bin/env python3
+
 import argparse
 from demandator import demandator
 
-parser = argparse.ArgumentParser()
-parser.add_argument("path", type=str, help="insert the path of the image to analyze")
-parser.add_argument("-v", "--verbose", action="count", default=0)
-args = parser.parse_args()
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, help="insert the path of the image to analyze")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+    args = parser.parse_args()
+    return args
 
-demandator.demandator(args.path, args.verbose)
+if __name__ == "__main__":
+    args = parse_arguments()
+    demandator.demandator(args.path, args.verbose)
