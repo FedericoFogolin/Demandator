f488b7afd91fe3cf765957b828a565ba364f02d6
Enrico Da Rodda <enricodarodda@hotmail.it>
2019-11-24 23:15:38 +0100

    Solve iss007 - Insert verbosity parameter

--- demandator/demandator.py
+++ demandator/demandator.py
@@ -1,8 +1,17 @@
 import requests
 
-def demandator (path):
+def demandator (path, verbose):
     url = 'https://fishidtest.herokuapp.com/api/test'
     files = {'file_field': open(path, 'rb')}
+    
+    if verbose >= 2:
+        print('Getting the image from the path  :  ' + path)
+        print('Sending the image to the API at  :  ' + url)
+    elif verbose == 1:
+        print('Sending the image ...')
+    
     r = requests.post(url, files=files)
+    if verbose >= 1:
+        print('Getting the results ...')
+    
     print(r.content)
-
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import argparse
 from demandator import demandator
 
 parser = argparse.ArgumentParser()
-parser.add_argument("path", help="insert the path of the image to analyze")
+parser.add_argument("path", type=str, help="insert the path of the image to analyze")
+parser.add_argument("-v", "--verbose", action="count", default=0)
 args = parser.parse_args()
 
-demandator.demandator(args.path)
+demandator.demandator(args.path, args.verbose)
