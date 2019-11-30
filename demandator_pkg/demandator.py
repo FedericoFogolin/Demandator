c1873548d15df69c141535d460d7d7d5c063830d
Lorenzo Pinto <lorenzo-pinto@hotmail.it>
2019-11-30 17:01:41 +0000

    solve iss11, solve iss09

--- demandator_pkg/demandator.py
+++ demandator_pkg/demandator.py
@@ -1,8 +1,8 @@
 import requests
+import ast
 
-def demandator (path, verbose):
+def demandator (path, verbose, n_results):
     url = 'https://fishidtest.herokuapp.com/api/test'
-    print(path)
     try:
         files = {'file_field': open(path, 'rb')}
     except FileNotFoundError:
@@ -17,5 +17,12 @@ def demandator (path, verbose):
     r = requests.post(url, files=files)
     if verbose >= 1:
         print('[INFO] Getting the results ...')
-    
-    print(r.content)
+    results = ast.literal_eval((r.content).decode("utf-8"))['results']
+    for count, i in enumerate (results):
+        result = i.split(',')
+	# solve string result if value is == 1.0 (really remote case)
+        print ('Result n.{} label:{} probability: {}'.format(result[0], result[1], result[2]))
+        # stop iterations on the based on number of results asked 
+        # (count + 1) because enumerate starts from 0
+        if (count+1) == n_results:
+            break
--- main.py
+++ main.py
@@ -7,9 +7,10 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=str, help="insert the path of the image to analyze")
     parser.add_argument("-v", "--verbose", action="count", default=0)
+    parser.add_argument("-n", "--n_results", type = int, default=5, help="insert number of results to show")
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = parse_arguments()
-    demandator.demandator(args.path, args.verbose)
+    demandator.demandator(args.path, args.verbose, args.n_results)
