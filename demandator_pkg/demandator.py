5c6f91d085c8c0190071b1620760e6f36f2fae8a
Lorenzo Pinto <lorenzo-pinto@hotmail.it>
2019-12-04 13:38:16 +0100

    refactor some lines of code

--- demandator_pkg/demandator.py
+++ demandator_pkg/demandator.py
@@ -7,16 +7,23 @@ def demandator (path, verbose, n_results, threshold):
         files = {'file_field': open(path, 'rb')}
     except FileNotFoundError:
         print('[ERROR] File not found. Make sure the path is correct and the file is available.')
-        return 
+        return
+    
+    # to remove and substitute with logging module
     if verbose >= 2:
         print('[INFO] Getting the image from the path  :  ' + path)
         print('[INFO] Sending the image to the API at  :  ' + url)
     elif verbose == 1:
         print('[INFO] Sending the image ...')
+    # till here
     
     r = requests.post(url, files=files)
+    
+    # to remove and substitute with logging module
     if verbose >= 1:
         print('[INFO] Getting the results ...')
+    # till here
+    
     results = ast.literal_eval((r.content).decode("utf-8"))['results']
     for count, i in enumerate (results):
         result = i.split(',')
@@ -24,6 +31,9 @@ def demandator (path, verbose, n_results, threshold):
         # check that prediction has accuracy >= of threshold (that is 0.0 as default)
         if float(result[2]) >= threshold:
             print('{}: {:.1%}'.format(result[1], float(result[2])))
+        elif float(result[2]) < threshold:
+            breakgit branch
+            
         # stop iterations on the based on number of results asked
         # (count + 1) because enumerate starts from 0
         if (count+1) == n_results:
