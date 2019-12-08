2ff0ce43d0de3e2079294738beae9e732d7d2d04
FabioDeBenetti <fabio.debenetti@student.h-farm.com>
2019-12-08 17:59:53 +0100

    Solved iss12 - Create database to save Picture for transfer learning of the model

--- demandator_pkg/demandator.py
+++ demandator_pkg/demandator.py
@@ -8,6 +8,16 @@ from math import sin, pi
 
 def demandator (path, verbose, n_results, threshold, plot):
     url = 'https://fishidtest.herokuapp.com/api/test'
+    global conn
+    global cursor
+    conn = sqlite3.connect('db_images.db')
+    cursor = conn.cursor()
+
+    def convert_to_binary_data(path):
+        with open(path, 'rb') as file:
+            blobim = file.read()
+        return blobim
+
     try:
         files = {'file_field': open(path, 'rb')}
     except FileNotFoundError:
@@ -33,6 +43,21 @@ def demandator (path, verbose, n_results, threshold, plot):
     data = []
     for count, i in enumerate (results):
         result = i.split(',')
+        #initiate the database and insert values in it
+        if count == 0:
+            accuracy = float(result[2])
+            blobim = convert_to_binary_data(path)
+            imhash = hashlib.sha256(blobim).hexdigest()
+
+            try:
+                cursor.execute("SELECT * FROM images")
+            except sqlite3.OperationalError:
+                cursor.execute('''CREATE TABLE images(image_hash TEXT NOT NULL, image_blob BLOB NOT NULL, prediction_accuracy REAL NOT NULL, PRIMARY KEY (imahe_hash))''')
+            finally:
+                if len(cursor.execute("SELECT * FROM images WHERE image_hash=?", [imhash]).fetchall()) == 1:
+                    cursor.execute("INSERT INTO images (image_hash, image_blob, prediction_accuracy) VALUES (?,?,?)", (imhash, blobim, accuracy))
+                    conn.commit()
+
 	# TODO solve string result if value is == 1.0 (really remote case)
         # check that prediction has accuracy >= of threshold (that is 0.0 as default)
         if float(result[2]) >= threshold:
